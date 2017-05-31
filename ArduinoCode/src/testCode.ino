49f8435c7b5b2954cb14ce44261e6e56daf5b9fa
Tim Streilein <tstreilein@atlassian.com>
2017-05-31 14:44:53 +0200

    blink ESP

--- ArduinoCode/src/testCode.ino
+++ ArduinoCode/src/testCode.ino
@@ -1,58 +1,13 @@
-
-#include <Servo.h>
-
-Servo bottom;
-Servo top;
-
-
-int pos_b = 0;
-const int servoMin_b = 12;
-const int servoMax_b = 150;
-const int servoPin_b = 9;
-const int servoDelay_b = 3;
-
-int pos_t = 0;
-const int servoMin_t = 70;
-const int servoMax_t = 150;
-const int servoPin_t = 10;
-const int servoDelay_t = 15;
-
-void setup()
-{
-  bottom.attach(servoPin_b);
-  top.attach(servoPin_t);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
-
-void loop()
-{
-  for (pos_b = servoMin_b; pos_b < servoMax_b; pos_b += 1)
-  {
-    bottom.write(pos_b);
-    delay(servoDelay_b);
-  }
-
-
-  for (pos_b = servoMax_b; pos_b >= servoMin_b; pos_b -= 1)
-  {
-    bottom.write(pos_b);
-    delay(servoDelay_b);
-  }
-
-
-
-  for (pos_t = servoMin_t; pos_t < servoMax_t; pos_t += 1)
-  {
-    top.write(pos_t);
-    delay(servoDelay_t);
-  }
-
-
-  for (pos_t = servoMax_t; pos_t >= servoMin_t; pos_t -= 1)
-  {
-    top.write(pos_t);
-    delay(servoDelay_t);
-  }
-
-
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+                                    // but actually the LED is on; this is because
+                                    // it is acive low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
